363d527c6ecab9af8ea7f0a0028b1c47a6658f13
estrela801 <rohenri0808@gmail.com>
2024-08-16 18:06:21 -0300

    feat: criando primeiras rotas

---
+++ rotas/adm.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',()=>{
+    res.send('pagina principal')
+    
+})
+
+router.get('/posts',(req,res)=>{
+   res.send('pagina de posts')
+    
+})
+
+router.get('/categorias',  (res,res)=>{
+    res.send('pagia de categoras')
+    
+})
+module.exports = router
