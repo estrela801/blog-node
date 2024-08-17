0e0fcc39755d8231a4ea4eb1d0495c3078bcab69
estrela801 <rohenri0808@gmail.com>
2024-08-17 11:09:57 -0300

    fix: arrumando a inicialização do handlebars feat: adicionando rotas de admin

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 // iniciando Módulos
     const express = require('express')
-    const handlebars = require('express-handlebars')
+    const { create } = require('express-handlebars')
+    const handlebars = create({defaultLayout:'main'})
     const bodyParser = require('body-parser')
     const app = express()
+    const admin = require('./rotas/adm')
     //const mongoose = require('mongoose')
 //Configurações
     // Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
     // HandleBars
-        app.engine('handlebars', handlebars({defaultLayout:'main'}))
+        app.engine('handlebars', handlebars.engine)
         app.set('view engine', 'handlebars')
     //Mongoose
         //Em breve
 //Rotas
-
+    app.use('/admin', admin)
 //Outros
 const PORTA = 3000
 app.listen(PORTA, ()=>{
