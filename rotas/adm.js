c667494c2d97a751fe044119a539286910bd5cdd
estrela801 <rohenri0808@gmail.com>
2024-08-19 17:05:39 -0300

    feat: criando um model e adicionando rotas para o mesmo

--- rotas/adm.js
+++ rotas/adm.js
@@ -1,20 +1,36 @@
 const express = require('express')
 const router = express.Router()
+const Categoria = require('../models/Categoria')
+
 
 router.get('/',(req,res)=>{
-    res.render('pages/adm')
+    res.render('admin/adm')
 })
 
 router.get('/posts',(req,res)=>{
    res.send('pagina de posts')
     
 })
-
+router.get('/categorias/add'    ,(req,res)=>{
+    res.render('admin/addcategoria')
+})
 router.get('/categorias',  (req,res)=>{
-    res.send('pagia de categoras')
+    res.render('admin/categorias')
 })
 router.get('/blog',(req,res)=>{
-    res.render('pages/blog')
+    res.render('admin/blog')
+})
+router.post('/categorias/add/',  (req,res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then( () => {
+        console.log('Categoria registrada');
+    }).catch(err=>{
+        console.log('Erro ao registrar nova categoria ->', err);
+    })
 })
 
 module.exports = router
