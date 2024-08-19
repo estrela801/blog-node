3c14eee454deaad4edf3d21c3295176a85d376e8
estrela801 <rohenri0808@gmail.com>
2024-08-19 17:04:40 -0300

    feat: aprendendo sobre middleware feat: conectando com o mongo

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const { create } = require('express-handlebars');
 const path = require('path');
 const app = express();
 const admin = require('./rotas/adm');
+const mongoose = require('mongoose')
 
 // Configurações
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use( (req,res,next) => {
+    console.log('oi eu sou um middleware');
+    next()
+})
 
 // HandleBars
 const handlebars = create({ 
@@ -17,15 +22,24 @@ app.set('view engine', 'handlebars');
 // app.set('views', path.join(__dirname, 'views', 'layout'));
 
 // Public
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+//Mongoose
+    mongoose.connect('mongodb://localhost/blogapp').then(()=>{
+        console.log('Conectado com sucesso');
+    }
+).catch(err=>{
+    console.log('Banco não conectado',err);
+    
+})
+mongoose.Promisse = global.Promisse
 // Rotas
 app.use('/admin', admin);
 app.get('/', (req, res) => {
     res.send('inicial');
 });
 app.get('/blog', (req, res) => {
-    res.render('pages/blog'); // Certifique-se de que 'pages/blog.handlebars' existe
+    res.render('admin/blog'); // Certifique-se de que 'pages/blog.handlebars' existe
 });
 
 // Outros
