91b226ce472557f0ea387703569a13839d7d5d68
estrela801 <rohenri0808@gmail.com>
2024-08-20 16:55:23 -0300

    fix: corrigindo erro de middleware

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
-app.use(flash)
+app.use(flash())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use( (req,res,next) => {
