6049be71ca2398c419800f0342e7d408506a0b7d
estrela801 <rohenri0808@gmail.com>
2024-08-27 18:06:46 -0300

    feat: criando a lógica de autenticação

---
+++ config/auth.js
@@ -0,0 +1,42 @@
+const localStrategy = require('passport-local').Strategy    
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Usuario = require('../models/Usuarios')
+
+module.exports = (passport) => {
+    passport.use(new localStrategy({usernameField: 'email', passwordField: 'senha'}, (email, senha, done) => {
+        Usuario.findOne({email: email}).then(usuario => {
+            if (!usuario) {
+                // console.log('Email não encontrado');
+                
+                return done(null, false, { mensagem: 'O usuário não existe' });
+            }
+
+            bcrypt.compare(senha, usuario.senha, (erro, batem) => {
+                if (batem) {
+                    // console.log('Senha correta');
+                    return done(null, usuario);
+                } else {
+                    // console.log('Senha incorreta');
+                    return done(null, false, { mensagem: 'Senha incorreta' });
+                }
+            });
+        }).catch(err => {
+            return done(err);
+        });
+    }));
+
+    passport.serializeUser((usuario, done) => {
+        done(null, usuario.id);
+    });
+
+    passport.deserializeUser(async   (id, done) => {
+        try {
+            const usuario = await Usuario.findById(id).lean();
+            done(null, usuario);
+        } catch (err) {
+            done(err, null);
+        }
+    }); 
+    
+}
